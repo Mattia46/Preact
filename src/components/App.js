125f7b45cb1ad39001287f724b38626869c804ca
Mattia46 <mattia.assogna@gmail.com>
2017-08-16 17:25:57 +0100

    created the basic structure of Preact

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+export function App () {
+    return (
+        <div class="app">
+            <h1> Hello Mattia </h1>
+        </div>
+    )
+}
+
+export default App;
